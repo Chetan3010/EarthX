f29b0d47ebee3a317c8228f36c280dcb751cf8df
“[chetan <[ckale2001@gmail.com]>
2024-07-01 21:54:48 +0530

    ⚒ Fixed interval on playerPause | playerResume event

--- events/player/playerPause.js
+++ events/player/playerPause.js
@@ -1,10 +1,15 @@
-const { EmbedBuilder, escapeMarkdown } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 const { botColor } = require("../../configs/config");
 const { pause } = require("../../configs/emojis");
 
 module.exports = {
 	name: 'playerPause',
 	async execute(queue, track, client) {
+		// Pause the interval updates
+		if (queue.metadata?.updateInterval) {
+			clearInterval(queue.metadata.updateInterval);
+			queue.metadata.updateInterval = null;
+		}
 		const res = await queue.metadata.channel.send({
 			embeds: [
 				new EmbedBuilder()
@@ -12,6 +17,6 @@ module.exports = {
 					.setDescription(`${pause} Player is paused for now.`)
 			]
 		});
-		queue.metadata.pauseMsge = res.id
+		queue.metadata.pauseMsge = res.id;
 	}
 }
--- events/player/playerResume.js
+++ events/player/playerResume.js
@@ -1,16 +1,43 @@
-const { EmbedBuilder, escapeMarkdown } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 const { botColor } = require("../../configs/config");
-const { resume } = require("../../configs/emojis");
+const { resume, cyanDot, bottomArrow } = require("../../configs/emojis");
 const { BOT_MSGE_DELETE_TIMEOUT } = require("../../helper/constants");
 const { errorLog } = require("../../configs/logger");
+const { getProgressBar } = require("../../helper/utils");
 
 module.exports = {
 	name: 'playerResume',
 	async execute(queue, track, client) {
 		try {
-			if (queue.metadata?.pauseMsge) queue.metadata.channel.messages.delete(queue.metadata.pauseMsge)
-			// Reset the flag
+			if (queue.metadata?.pauseMsge) queue.metadata.channel.messages.delete(queue.metadata.pauseMsge);
+
 			if (queue.metadata.interaction.guild.members.me?.voice?.channel.members.size === 1) return; // Ignore this resume event
+			// Restart the interval updates
+			const interval = setInterval(async () => {
+				if (!queue.currentTrack || queue.currentTrack.id !== queue.metadata.currentTrackId) {
+					clearInterval(interval);
+					return;
+				}
+
+				try {
+                    const nowPlayingMessage = await queue.metadata.channel.messages.fetch(queue.metadata.nowPlaying);
+					const embed = nowPlayingMessage.embeds[0];
+
+                    const progressBarFieldIndex = embed.fields.findIndex(field => field.name === `${cyanDot} Progress ${bottomArrow}`);
+					if (progressBarFieldIndex !== -1) {
+						embed.fields[progressBarFieldIndex].value = getProgressBar(queue.node);
+					}
+
+					await nowPlayingMessage.edit({ embeds: [embed] });
+				} catch (error) {
+					console.log(error);
+					clearInterval(interval);
+				}
+			}, 10000);
+
+			queue.metadata.updateInterval = interval;
+
+			// Send resume message
 
 			await queue.metadata.channel.send({
 				embeds: [
@@ -18,9 +45,9 @@ module.exports = {
 						.setColor(botColor)
 						.setDescription(`${resume} Player is resumed now.`)
 				]
-			}).then(msge => setTimeout(() => msge.delete(), BOT_MSGE_DELETE_TIMEOUT))
+			}).then(msge => setTimeout(() => msge.delete(), BOT_MSGE_DELETE_TIMEOUT));
 		} catch (error) {
-			errorLog('An error occured with player event!')
+			errorLog('An error occurred with player event!');
 			console.log(error);
 		}
 	}
